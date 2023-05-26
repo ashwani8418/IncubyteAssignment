9e14b44b7dd2d904ed971526158173b1dcca49f4
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2023-05-26 17:15:07 +0530

    Add method updated to check sum of number separated by commas TestCase 3 passing String number("1,2") separated by commas and expected result is 3

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -31,7 +31,7 @@ public class AppTest
     }
 
     /**
-     * Rigourous Test :-)
+     * Rigorous Test :-)
      */
     public void testApp()
     {
@@ -39,10 +39,13 @@ public class AppTest
         assertEquals(0, Add(""));
 
 //        TestCase 2 passing "1" to Add method and expected result is 1;
-        assertEquals(9, Add("9"));
+        assertEquals(1, Add("1"));
 
 //        TestCase 3 passing String number("1,2") separated by commas and expected result is 3
         assertEquals(3, Add("1,2"));
+
+//    TestCase 4 passing String number("9099,10900,1,4859") unknown amount of number separated by commas and expected result is 24859
+        assertEquals(24859, Add("9099,10900,1,4859"));
     }
 
 }
