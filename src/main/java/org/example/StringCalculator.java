d212a794cfd30a7ce01fd0124555142074155ca9
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2023-07-09 18:46:52 +0530

    Test 7 twoAnyDelimiterSeparatedReturnSum testcase passed

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class StringCalculator
 {
-    private static final String delimeter = "[^0-9-]";
+    private static final String delimiter = "[^0-9-]";
     public static int add(String numbers){
         if(numbers.length() == 1){
             return Integer.parseInt(numbers);
         } else if (numbers.length() > 1) {
 
-            String[] num = numbers.split(delimeter);
+            String[] num = numbers.split(delimiter);
             List<Integer> negativeNumber = new ArrayList<>();
             int sum = 0;
             for (String s : num) {
